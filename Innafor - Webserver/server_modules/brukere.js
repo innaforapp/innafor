26b7bb83aa39b503434294c9787fb9670713fa88
Kristian Frøitland <froitland@mail.com>
2019-04-09 10:27:37 +0200

    node_modules

--- Innafor - Webserver/server_modules/brukere.js
+++ Innafor - Webserver/server_modules/brukere.js
@@ -142,7 +142,7 @@ router.post("/registrer/", authorize, nameToLowerCase, emailToLowerCase, existin
     try {
         let regUser = await db.any(regUserQuery);
 
-        /*
+        
         //==Sender epost til bruker===
                 // create reusable transporter object using the default SMTP transport
                 let transporter = nodemailer.createTransport({
@@ -150,8 +150,8 @@ router.post("/registrer/", authorize, nameToLowerCase, emailToLowerCase, existin
                   port: 465,
                   secure: true, // true for 465, false for other ports
                   auth: {
-                    user: "process.env.NOREPLY_MAIL", // generated ethereal user
-                    pass: "process.env.NOREPLY_PASSORD" // generated ethereal password
+                    user: process.env.NOREPLY_MAIL, // generated ethereal user
+                    pass: process.env.NOREPLY_PASSORD // generated ethereal password
                   }
                 });
 
@@ -173,7 +173,7 @@ router.post("/registrer/", authorize, nameToLowerCase, emailToLowerCase, existin
                 // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
                 // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
         //=======================
-        */
+        
 
         res.status(200).json({
             event: `toatsUserRegister.open();`
